907588f455bec4b21f65df07ef9629dd54a3bf29
Nathan nascimento <nathasilvanascimento@gmail.com>
2022-03-07 15:14:31 -0300

    atualizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import './scss/style.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 
 import { Home } from './pages/Home';
 import { OrphanagesMap } from './pages/OrphanagesMap';
-import { Orphanage } from './pages/Orphanage';
 import { CreateOrphanage } from './pages/CreateOrphanage';
 import { ViewOrphanage } from './pages/ViewOrphanage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/create" element={<CreateOrphanage />} />
       <Route path="/orphanages" element={<OrphanagesMap />} />
       <Route path="/orphanages/:id" element={<ViewOrphanage />} />
      </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
